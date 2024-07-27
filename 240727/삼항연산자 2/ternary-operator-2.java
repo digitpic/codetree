0e71f27f48aace2a0f6a83c01134c912e1a01faa
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-27 17:54:30 +0900

    Title: 삼항연산자 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240727/삼항연산자 2/ternary-operator-2.java
+++ 240727/삼항연산자 2/ternary-operator-2.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        a == 1? System.out.println("t") : System.out.println("f");
+        System.out.println(a == 1 ? 't' : 'f';);
     }
 }
