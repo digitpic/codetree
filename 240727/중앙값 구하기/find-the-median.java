07a8ace72378dff38186bdee91a428ef3ab1f162
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-27 18:57:53 +0900

    Title: 중앙값 구하기, Time: 153ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240727/중앙값 구하기/find-the-median.java
+++ 240727/중앙값 구하기/find-the-median.java
@@ -6,21 +6,42 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if (a >= b) {
-            if (c >= a) {
+        // a가 클 때
+        if (a > b) {
+            // c가 클 때
+            if (c > a) {
+                // 중앙값은 a
+                System.out.println(a);
+            }
+            // a가 클 때
+            else {
+                // 중앙값은 c
+                System.out.println(c);
+            }
+        }
+        // b가 클 때
+        else if (b > a) {
+            // c가 클 때
+            if (c > b) {
+                // 중앙값은 b
+                System.out.println(b);
+            }
+            // b가 클 때
+            else {
+                // 중앙값은 c
+                System.out.println(c);
+            }
+        }
+        else if (a > c) {
+            if (b > a) {
+                // 중앙값은 a
                 System.out.println(a);
             }
             else {
-                System.out.println(c);
-            }
-        }
-        else {
-            if (c >= b) {
+                // 중앙값은 b
                 System.out.println(b);
             }
-            else {
-                System.out.println(c);
-            }
         }
+
     }
 }
