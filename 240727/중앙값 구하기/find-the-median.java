7f94152583350f6b3eec210d18045d2f2917b481
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-27 19:19:02 +0900

    Title: 중앙값 구하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240727/중앙값 구하기/find-the-median.java
+++ 240727/중앙값 구하기/find-the-median.java
@@ -8,26 +8,34 @@ public class Main {
 
         // a가 클 때
         if (a > b) {
+            // c > a > b
             if (c > a) {
                 System.out.println(a);
             }
+            // a > b > c
             else if (b > c) {
                 System.out.println(b);
             }
+            // a > c > b
             else {
                 System.out.println(c);
             }
         }
+        // b가 클 때
         else {
-            if (c > a) {
+            // b > a > c
+            if (a > c) {
                 System.out.println(a);
             }
-            else if (b < a) {
+            // c > b > a
+            else if (c > b) {
                 System.out.println(b);
             }
+            // b > c > a
             else {
                 System.out.println(c);
             }
+            
         }
 
     }
