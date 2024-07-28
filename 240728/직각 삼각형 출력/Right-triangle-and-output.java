21da52dd830c1c3725bc26ea425b816fb8595f92
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-28 21:52:03 +0900

    Title: 직각 삼각형 출력, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240728/직각 삼각형 출력/Right-triangle-and-output.java
+++ 240728/직각 삼각형 출력/Right-triangle-and-output.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
 
-        // [1] n만큼 반복
-        for (int i = 0, k = 0; i < n; i++, k += 2) {
-
-            // [2] 1, 3, 5, ...만큼 반복
-            for (int j = 0; j <= k; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 2 * i + 1; j++)
                 System.out.print("*");
-            }
-            
             System.out.println();
         }
-
     }
 }
