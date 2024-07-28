0125ae7e610c1abd59d4e1c57dc59af47f33f2fa
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-28 11:37:11 +0900

    Title: 4번의 크기 비교, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240728/4번의 크기 비교/4-time-comparison.java
+++ 240728/4번의 크기 비교/4-time-comparison.java
@@ -1,41 +1,20 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
-        int e = sc.nextInt();
-        if (a > b) {
-            System.out.println(1);
-        } 
-        else {
-            System.out.println(0);
-        }
 
-        if (a > c) {
-            System.out.println(1);
-        } 
-        else {
-            System.out.println(0);
-        }
+        int a, b, c, d, e;
+        
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        e = sc.nextInt();
 
-        if (a > d) {
-            System.out.println(1);
-        } 
-        else {
-            System.out.println(0);
-        }
-
-        if (a > e) {
-            System.out.println(1);
-        } 
-        else {
-            System.out.println(0);
-        }
-
-
-
+        System.out.println(a > b ? 1 : 0);
+        System.out.println(a > c ? 1 : 0);
+        System.out.println(a > d ? 1 : 0);
+        System.out.println(a > e ? 1 : 0);
     }
 }
