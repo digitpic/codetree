fe0a56221619a0613110fb7d7887035277a4c94a
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-28 22:28:11 +0900

    Title: 건너뛰고 별표 출력하기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240728/건너뛰고 별표 출력하기/skip-and-print-a-star.java
+++ 240728/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
+        // n번 반복
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("*");
@@ -10,6 +12,7 @@ public class Main {
             System.out.println("\n");
         }
 
+        // n-1번 반복
         for (int i = n-1; i > 0; i--) {
             for (int j = i; j > 0; j--) {
                 System.out.print("*");
