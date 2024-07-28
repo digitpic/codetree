fffc9c4f6622cc8d461b36757cc50194c131bafc
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-28 11:44:06 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240728/두 사람/two-person.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int a1 = sc.nextInt();
+        String s1 = sc.next();
+        int a2 = sc.nextInt();
+        String s2 = sc.next();
+
+        System.out.println((a1 > 19 || a2 > 19) && (s1.equals("M") || s2.equals.("M")) ? 1 : 0);
+    }
+}
