9cbd1a4069b3892347d3ffdc9849ab6e3e8800ec
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-29 15:41:18 +0900

    Title: 별표 출력하기 10, Time: 219ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240729/별표 출력하기 10/print-star-10.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // n*2번 반복 
+        for (int i = 0; i < n*2; i++) {
+
+            // [1] 홀수
+            // 1, 2, 3
+            if (i % 2 == 1) {
+                for (int j = 0; j < n - (i - 1) / 2; j++) {
+                    System.out.print("* ");
+                }
+            }
+
+            // [2] 짝수
+            // 3, 2, 1
+            else {
+                for (int j = 0; j < 1 + (i / 2); j++) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+}
