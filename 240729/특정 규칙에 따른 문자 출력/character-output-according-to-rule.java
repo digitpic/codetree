c76bd75b45ba923be112c9211130e0adf2d93033
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-29 13:09:39 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240729/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // [1] 위
+        // n 번 반복
+        for (int i = 0; i < n; i++) {
+            // [1][1] 공백
+            // n=3: 2, 1, 0
+            // n-1-i
+            for (int j = 0; j < n-1-i; j++) {
+                System.out.print("  ");
+            }
+            // [1][2] 문자
+            // n=3: 1, 2, 3
+            // i+1
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("@ ");
+            }
+            // [1][3] 줄내림
+            System.out.println();
+        } // end[1]
+
+        // [2] 아래
+        // n-1 번 반복
+        for (int i = 0; i < n-1; i++) {
+            // [2][1] 문자
+            // n=3: 2, 1
+            // n-1-i
+            for (int j = 0; j < n-1-i; j++) {
+                System.out.print("@ ");
+            }
+            // [2][2]
+            System.out.println();
+        } // end[2]
+
+    } // endMethod
+}
