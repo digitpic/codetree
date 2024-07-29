29d8ccedb037548f1d51ac343f6f6477b153f268
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-29 12:11:59 +0900

    Title: 별 그리기, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240729/별 그리기/star-drawing.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // [1] 위
+        // n 번 반복
+        for (int i = 0; i < n; i++) {
+            
+            // [1][1] 공백
+            // n = 3: 2, 1, 0
+            // n - 1 - i
+            for (int j = 0; j < n-1-i; j++) {
+                System.out.print(" ");
+            }
+
+            // [1][2] 별
+            // n = 3: 1, 3, 5
+            // (n - 2) + (2 * i)
+            for (int j = 0; j < (2*i) + (n-2); j++ ) {
+                System.out.print("*");
+            }
+
+            // [1][3] 줄내림
+            System.out.println();
+
+        } // end[1]
+
+        // [2] 아래
+        // n-1 번 반복
+        for (int i = 0; i < n-1; i++) {
+
+            // [2][1] 공백
+            // n = 3: 1, 2
+            // n-2+i
+            for (int j = 0; j < n-2+i; j++) {
+                System.out.print(" ");
+            }
+
+            // [2][2] 별
+            // n = 3: 3, 1
+            // n - (2*i)
+            for (int j = 0; j < n-(2*i); j++) {
+                System.out.print("*");
+            }
+
+            // [2][3] 줄내림
+            System.out.println();
+            
+        } // end[2]
+
+    } // endMain
+}
