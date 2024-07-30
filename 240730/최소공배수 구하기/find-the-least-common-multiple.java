a061013ce64361bb8c21c12fbf949564018cc49c
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-30 16:43:09 +0900

    Title: 최소공배수 구하기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240730/최소공배수 구하기/find-the-least-common-multiple.java
+++ 240730/최소공배수 구하기/find-the-least-common-multiple.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
+
 public class Main {
-    public static void solution() {
+    
+    public static void findLCM(int n, int m) {
+        int gcd = 0;
+        for(int i = 1; i <= Math.min(n, m); i++) {
+            if(n % i == 0 && m % i == 0)
+                gcd = i;
+        }
+    
+        System.out.print(n * m / gcd);
+    }
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int max = Math.max(n, m);
-        int result = max;
-        for (;;) {
-            if (result % n == 0 && result % m == 0) {
-                break;
-            }
-            result += max;
-        }
-        System.out.println(result);
-    }
-    public static void main(String[] args) {
-        solution();
+
+        findLCM(n, m);
     }
 }
