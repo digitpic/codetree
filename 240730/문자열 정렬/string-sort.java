ea9b1ae71e884e604bf49d0cb65a1b180a5fbf67
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-30 15:48:44 +0900

    Title: 문자열 정렬, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240730/문자열 정렬/string-sort.java
+++ 240730/문자열 정렬/string-sort.java
@@ -1,10 +1,14 @@
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // 입력
         String str = sc.next();
+
+        // 문자 사전순 정렬
         char[] chars = str.toCharArray();
         Arrays.sort(chars);
         String sortedStr = new String(chars);
