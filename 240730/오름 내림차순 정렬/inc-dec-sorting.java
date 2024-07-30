e1b39371e88ea8d2526d12b6223614389811648d
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-30 15:46:44 +0900

    Title: 오름 내림차순 정렬, Time: 341ms, Memory: 16MB, Status: Passed - CodeTree

--- 240730/오름 내림차순 정렬/inc-dec-sorting.java
+++ 240730/오름 내림차순 정렬/inc-dec-sorting.java
@@ -1,25 +1,30 @@
+import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력:
         int n = sc.nextInt();
-        Integer[] arr = new Integer[n];
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++)
+            nums[i] = sc.nextInt();
 
-        for (int i=0; i<n; i++) {
-            arr[i]=sc.nextInt();
-        }
+        // 오름차순 정렬
+        Arrays.sort(nums);
 
-        Arrays.sort(arr);
-        for (int i=0; i<n; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        for(int i = 0; i < n; i++)
+            System.out.print(nums[i] + " ");
         System.out.println();
 
-        Arrays.sort(arr, Collections.reverseOrder());
-        for (int i=0; i<n; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        // 내림차순 정렬
+        Integer[] nums2 = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        Arrays.sort(nums2, Collections.reverseOrder());
+
+        for(int i = 0; i < n; i++)
+            System.out.print(nums2[i] + " ");
+        System.out.println();
     }
 }
