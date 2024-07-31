b7eaa909365bdbdaffdfb69d4a15160f18efa06e
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-08-01 08:17:34 +0900

    Title: 그 전 알파벳, Time: 117ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240801/그 전 알파벳/before-the-alphabet.java
+++ 240801/그 전 알파벳/before-the-alphabet.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char c = sc.next().charAt(0);
-        System.out.println( (char)((int)c - 1) );
+        int n = (int)c - 1;
+        char result = (char)n;
+
+        System.out.println(result);
     }
 }
