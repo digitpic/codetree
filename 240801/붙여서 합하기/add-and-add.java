520830becf87992aaa5675f10403e7daaf42bcb4
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-08-01 08:54:47 +0900

    Title: 붙여서 합하기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240801/붙여서 합하기/add-and-add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String s1 = sc.next();
+        String s2 = sc.next();
+
+        String AB = s1 + s2;
+        String BA = s2 + s1;
+
+        int result = Integer.parseInt(AB) + Integer.parseInt(BA);
+
+        System.out.println(result);
+        
+    }
+}
