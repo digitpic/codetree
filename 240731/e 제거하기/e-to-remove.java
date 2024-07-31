58971e9ac6fdb899250d52795b347b5662ad08fc
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-31 22:04:55 +0900

    Title: e 제거하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240731/e 제거하기/e-to-remove.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int index = str.indexOf("e");
+        String result = str.substring(0, index) + str.substring(index+1);
+        System.out.println(result);
+    }
+}
