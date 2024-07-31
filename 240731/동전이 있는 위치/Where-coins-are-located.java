f12923e25d81f30f24f78f064b43e1bcf5a5fe8a
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-07-31 09:11:44 +0900

    Title: 동전이 있는 위치, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240731/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] board = new int[n][n];
+
+        for (int i = 0; i < m; i++) {
+            int x = sc.nextInt()-1;
+            int y = sc.nextInt()-1;
+            board[x][y] = 1;
+        }
+
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
