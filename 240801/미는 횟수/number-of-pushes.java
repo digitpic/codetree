fcd6bdb617efde5edc8d5d293d48554f775a53c1
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-08-01 21:52:24 +0900

    Title: 미는 횟수, Time: 835ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240801/미는 횟수/number-of-pushes.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.next();
+        String B = sc.next();
+        
+        int cnt = 0;
+
+        int len = A.length();
+
+        // 같지 않을 때 반복
+        while (!A.equals(B)) {
+            A = A.substring(1) + A.substring(0, 1);
+            cnt++;
+            if (cnt > len) {
+                cnt = -1;
+            }
+        }
+        
+        System.out.println(cnt);
+
+    }
+}
