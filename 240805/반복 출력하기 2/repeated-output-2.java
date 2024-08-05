498f5cbe395a57ffa51d4bfc60a3521812c66596
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-08-05 21:42:06 +0900

    Title: 반복 출력하기 2, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240805/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void printString(int n) {
+        if(n == 0)
+            return;
+
+        printString(n - 1);
+        System.out.println("HelloWorld");
+    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        printString(n);
+	}
+}
