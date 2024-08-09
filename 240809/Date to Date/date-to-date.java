35e30b5ca7361d97a698238fe64b2f7fa4ab0442
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-08-09 23:11:31 +0900

    Title: Date to Date, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240809/Date to Date/date-to-date.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        
+        System.out.println(numOfDays(m2, d2) - numOfDays(m1, d1) + 1);
+    }
+
+    public static int numOfDays(int m, int d) {
+        int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int totalDays = 0;
+        
+        for(int i = 1; i < m; i++)
+            totalDays += days[i];
+        
+        totalDays += d;
+        
+        return totalDays;
+    }
+    
+}
