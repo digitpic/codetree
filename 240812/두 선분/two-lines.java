02b0028c8b19ea67b070ec626c08d13a1699b284
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-08-12 14:18:08 +0900

    Title: 두 선분, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240812/두 선분/two-lines.java
+++ 240812/두 선분/two-lines.java
@@ -1,22 +1,32 @@
 import java.util.Scanner;
+
 public class Main {
+    public static int x1, x2, x3, x4;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int x1 = sc.nextInt();
-        int x2 = sc.nextInt();
-        int x3 = sc.nextInt();
-        int x4 = sc.nextInt();
+        x1 = sc.nextInt();
+        x2 = sc.nextInt();
+        x3 = sc.nextInt();
+        x4 = sc.nextInt();
 
         sc.close();
-
-        if (x1 <= x3 && x3 <= x2 ||
-            x3 <= x1 && x1 <= x4 ||
-            x1 <= x3 && x4 <= x2 ||
-            x3 <= x1 && x2 <= x2)
-            System.out.println("intersecting");
-        else 
-            System.out.println("nonintersecting");
-
+        
+        // 겹치는지를 확인
+        if(intersecting(x1, x2, x3, x4))
+            System.out.print("intersecting");
+        else
+            System.out.print("nonintersecting");
     }
+
+    public static boolean intersecting(int x1, int x2, int x3, int x4) {
+        // 겹치지 않는 경우
+        if(x2 < x3 || x4 < x1)
+            return false;
+        // 나머지는 전부 겹치는 경우
+        else
+            return true;
+    }
+
 }
