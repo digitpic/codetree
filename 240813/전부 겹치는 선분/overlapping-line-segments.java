fa26afdb19ff502c8712d68f97302010d49e860a
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-08-13 14:45:10 +0900

    Title: 전부 겹치는 선분, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240813/전부 겹치는 선분/overlapping-line-segments.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final int INT_MAX = Integer.MAX_VALUE;
+
+        int x1, x2;
+        int maxX1, int minX2 = INT_MAX;
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        // 시작점 중 가장 뒤에 있는 좌표와 끝점 중 가장 앞에 있는 점의 좌표를 확인
+        for(int i = 0; i < n; i++) {
+            x1 = sc.nextInt();
+            x2 = sc.nextInt();
+
+            maxX1 = Math.max(maxX1, x1);
+            minX2 = Math.min(minX2, x2);
+        }
+
+        sc.close();
+        
+        // 어느 한 선분이라도 시작점이 다른 선분의 끝점보다 뒤에 온다면
+        // 선분이 전부 겹칠 수 없음
+        if(minX2 >= maxX1)
+            System.out.print("Yes");
+        else
+            System.out.print("No");
+            
+    }
+}
