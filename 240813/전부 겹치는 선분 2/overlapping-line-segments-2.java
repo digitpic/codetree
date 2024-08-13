9da05bc35351814e25e2a34b0fe9ed29cd0d2f0a
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-08-13 14:57:18 +0900

    Title: 전부 겹치는 선분 2, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240813/전부 겹치는 선분 2/overlapping-line-segments-2.java
+++ 240813/전부 겹치는 선분 2/overlapping-line-segments-2.java
@@ -11,23 +11,26 @@ public class Main {
             x1[i] = sc.nextInt();
             x2[i] = sc.nextInt();
         }
+
+        sc.close();
         
         // 각 선분 제거
         for (int i = 0; i < n; i++) {
 
-            int max_x1 = Integer.MIN_VALUE;
-            int min_x2 = Integer.MAX_VALUE;
+            int maxX1 = Integer.MIN_VALUE;
+            int minX2 = Integer.MAX_VALUE;
             
             for (int j = 0; j < n; j++) {
                 
+                // i 번째 선분 제거
                 if (i == j) 
                     continue;
 
-                max_x1 = Math.max(max_x1, x1[j]);
-                min_x2 = Math.min(min_x2, x2[j]);
+                maxX1 = Math.max(maxX1, x1[j]);
+                minX2 = Math.min(minX2, x2[j]);
             }
             
-            if (max_x1 <= min_x2) {
+            if (maxX1 <= minX2) {
                 System.out.println("Yes");
                 return;
             }
