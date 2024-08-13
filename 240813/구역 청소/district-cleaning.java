5a6bd1dc3f92482c4181c372f958ee6a0959b5e9
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-08-13 10:59:35 +0900

    Title: 구역 청소, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240813/구역 청소/district-cleaning.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int MAX=100+100;
+        int[] board = new int[MAX+1];
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        sc.close();
+
+        for (int i=a; i<=b; i++)
+            board[i] = 1;
+
+        for (int i=c; i<=d; i++)
+            board[i] = 1;
+
+        int cnt = 0;
+        for (int i=1; i<=Math.max(b, d); i++)
+            if (board[i] == 1)
+                cnt++;
+
+        System.out.println(cnt-1);
+
+    }
+}
