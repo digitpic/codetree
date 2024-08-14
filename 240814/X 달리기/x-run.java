cc8e188546565832fa1ce47764fd1f07f656fbc7
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-08-14 21:29:52 +0900

    Title: X 달리기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240814/X 달리기/x-run.java
+++ 240814/X 달리기/x-run.java
@@ -1,42 +1,40 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력
         int x = sc.nextInt();
 
-        int speed = 0, meter = 0, time = 0;
+        int t = 0;
+        int leftDist = x;
+        int v = 1;
 
-        while (meter < x) {
+        while(true) {
+            leftDist -= v;
+            t++;
 
-            // 시간 증가
-            time++;
+            if(leftDist == 0)
+                break;
 
-            // 속도 증가
-            if ((meter + speed + 1) <= x && 
-                (x - (meter + speed + 1)) >= (speed + 1) * (speed + 2) / 2) {
-                speed++;
+            // 속도를 더 높여도 괜찮은지 결정합니다.
+            // 속도를 1 더 높이면, (v + 1) + v + (v - 1) + ... + 2 + 1 만큼 이동하게 됩니다.
+            // 즉, 남은 거리가 (v + 1) * (v + 2) / 2 보다 크거나 같아야 합니다.
+            if(leftDist >= (v + 1) * (v + 2) / 2)
+                v++;
+            
+            // 속도를 유지해도 괜찮은지 결정합니다.
+            // 속도를 유지하면, v + (v - 1) + ... + 2 + 1 만큼 이동하게 됩니다.
+            // 즉, 남은 거리가 v * (v + 1) / 2 보다 크거나 같아야 합니다.
+            else if(leftDist >= v * (v + 1) / 2) {
+                // 아무 것도 하지 않습니다.
             }
 
-            // 속도 유지
-            else if ((meter + speed) < x &&
-                (x - (meter + speed)) >= (speed * (speed - 1)) / 2) {
-                
-            }
-
-            // 속도 감소
-            else {
-                speed--;
-            }
-
-            // 이동
-            meter += speed;
-
+            // 위 둘을 만족하지 못하면 반드시 속도를 줄여야만 합니다.
+            else
+                v--;
         }
         
-        System.out.println(time);
-        
+        System.out.print(t);
     }
 }
