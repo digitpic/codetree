0be2f268dbf6c02c20c27a75f117d2b7714767eb
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-08-14 19:38:17 +0900

    Title: 비둘기와 전기줄, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240814/비둘기와 전기줄/pigeons-and-electric-cords.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static final int UNDEFINED = -1;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] pos = new int[n];
+        int[] pigeon = new int[n];
+        int[] moveDir = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            pigeon[i] = sc.nextInt() - 1;
+            moveDir[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++)
+            pos[i] = UNDEFINED;
+        
+        int moveCnt = 0;
+        for (int i = 0; i < n; i++) {
+
+            if( pos[pigeon[i]] == UNDEFINED) 
+                pos[pigeon[i]] = moveDir[i];
+            
+            else if (pos[pigeon[i]] != moveDir[i]) {
+                pos[pigeon[i]] = moveDir[i];
+                moveCnt++;
+            }
+
+        }
+
+        System.out.print(moveCnt);
+
+    }
+}
