43ce9a4599cf7d16b4225c9f9d83ccc5576903b6
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-08-16 23:55:41 +0900

    Title: 아름다운 수, Time: 241ms, Memory: 12MB, Status: Passed - CodeTree

--- 240816/아름다운 수/beautiful-number.java
+++ 240816/아름다운 수/beautiful-number.java
@@ -41,18 +41,25 @@ public class Main {
 
     public static boolean isBeautiful() {
 
+        // 현재 수에 따른 i 증가
+        // 1인 경우 +1
+        // 2인 경우 +2
+        // 3인 경우 +3
+        // 4인 경우 +4
         for(int i = 0; i < n; i += numList.get(i)) {
 
+            // 범위 검증
             if(i + numList.get(i) - 1 >= n)
                 return false;
 
+            // 숫자 개수 검증
             for(int j = i; j < i + numList.get(i); j++)
                 if(numList.get(j) != numList.get(i))
                     return false;
         }
 
         return true;
-        
+
     }
 
 }
