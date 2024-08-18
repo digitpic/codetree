083f0af1b9b27dbfc0dd6a63c9f7c59fa26069a3
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2024-08-18 17:13:02 +0900

    Title: 겹치지 않게 선분 고르기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240818/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 240818/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -13,9 +13,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 선분의 개수 입력 받기
         n = sc.nextInt();
         
-        // 입력
+        // 선분의 시작 좌표, 끝 좌표 입력 받기
         for (int i=0; i<n; i++) {
             int x1 = sc.nextInt();
             int x2 = sc.nextInt();
@@ -23,28 +25,30 @@ public class Main {
             lines[i] = new Line(x1, x2);
         }
 
+        // 겹치는 선분의 최대 값 구하기
         getMaxDuplication(0);
 
+        // 겹치는 선분의 최대 값 출력
         System.out.println(ans);
     }
 
     static void getMaxDuplication(int cnt) {
         
-        // 다 돌았으면
+        // 다 돌았다면
         if (cnt == n) {
-            // 겹치는지 확인
+            // 겹치지 않고 뽑을 수 있는 선분 개수 계산
             int max = 0;
             for (int i=0; i<n; i++) {
                 for (int j=i+1; j<n; j++) {
-                    // 겹친다면
-                    if (isDuplicated(lineList.get(i), lineList.get(j))) {
-                        // 정답 증가
+                    // 겹치지 않는다면
+                    if (!isOverlapped(lineList.get(i), lineList.get(j))) {
+                        // 겹치지 않는 선분 개수 추가
                         max++;
                     }
                 }
             }
 
-            // 겹치는 개수 확인
+            // 현재 기준 겹치는 선분의 최대 개수 ans 에 저장
             ans = Math.max(ans, max);
 
             // 퇴각
@@ -60,14 +64,14 @@ public class Main {
 
     }
 
-    static boolean isDuplicated(Line line1, Line line2) {
+    static boolean isOverlapped(Line line1, Line line2) {
         // 겹치지 않는 경우
         if (line1.x2 < line2.x1 || line2.x2 < line1.x1)
             return false;
         
         // 나머지는 겹치는 경우
         return true;
-    } 
+    }
 
 }
 
