16d5686de9089a88bc7ae36993c3243313735314
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-06-22 19:18:13 +0900

    Title: 아름다운 수, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250622/아름다운 수/beautiful-number.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    private static int n;
+    private static int answer;
+    private static List<Integer> numbers = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        find(0);
+        System.out.println(answer);
+    }
+
+    private static void find(int count) {
+        if (count == n) {
+            if (isBeautiful()) {
+                answer++;
+            }
+            return;
+        }
+
+        for (int i = 1; i <= 4; i++) {
+            numbers.add(i);
+            find(count + 1);
+            numbers.remove(numbers.size() - 1);
+        }
+    }
+
+    private static boolean isBeautiful() {
+        for (int i = 0; i < n; i += numbers.get(i)) {
+            if (n <= i + numbers.get(i) - 1) {
+                return false;
+            }
+            
+            for (int j = i; j < i + numbers.get(i); j++) {
+                if (numbers.get(i) != numbers.get(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
