3bb60f95164eb352b651c403c6bcd1d4638fce9c
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-06-26 15:08:01 +0900

    Title: 갈 수 있는 곳들, Time: 103ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250626/갈 수 있는 곳들/places-can-go.java
@@ -0,0 +1,101 @@
+import java.util.*;
+import java.io.*;
+
+class Pair {
+    public int x;
+    public int y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    private static final int MAX = 100;
+
+    private static int n;
+    private static int k;
+    private static int[][] grid;
+    private static int[][] starts;
+    private static int[][] visited = new int[MAX][MAX];
+    private static Queue<Pair> queue = new LinkedList<>();
+    private static int count;
+
+    public static void main(String[] args) throws IOException {
+        input();
+
+        for (int row = 0; row < k; row++) {
+            push(starts[row][0], starts[row][1]);
+            count++;
+        }
+
+        bfs();
+
+        output();
+    }
+
+    private static void push(int x, int y) {
+        visited[x][y] = 1;
+        queue.add(new Pair(x, y));
+    }
+
+    private static void bfs() {
+        while (!queue.isEmpty()) {
+            Pair current = queue.poll();
+            int x = current.x;
+            int y = current.y;
+
+            int[] dx = new int[]{0, 1, 0, -1};
+            int[] dy = new int[]{1, 0, -1, 0};
+
+            for (int direction = 0; direction < 4; direction++) {
+                int newX = x + dx[direction];
+                int newY = y + dy[direction];
+
+                if (isMovable(newX, newY)) {
+                    push(newX, newY);
+                    count++;
+                }
+            }
+        }
+    }
+
+    private static boolean isMovable(int x, int y) {
+        return isInRange(x, y)
+                && visited[x][y] == 0
+                && grid[x][y] == 0;
+    }
+
+    private static boolean isInRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+
+    private static void input() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokens = new StringTokenizer(reader.readLine());
+
+        n = Integer.parseInt(tokens.nextToken());
+        k = Integer.parseInt(tokens.nextToken());
+
+        grid = new int[n][n];
+
+        for (int row = 0; row < n; row++) {
+            tokens = new StringTokenizer(reader.readLine());
+            for (int column = 0; column < n; column++) {
+                grid[row][column] = Integer.parseInt(tokens.nextToken());
+            }
+        }
+
+        starts = new int[k][2];
+        for (int row = 0; row < k; row++) {
+            tokens = new StringTokenizer(reader.readLine());
+            starts[row][0] = Integer.parseInt(tokens.nextToken()) - 1;
+            starts[row][1] = Integer.parseInt(tokens.nextToken()) - 1;
+        }
+    }
+
+    private static void output() {
+        System.out.println(count);
+    }
+}
