03c94b2b108ac2bdd559819cc94c032c86e39159
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-06-26 13:36:26 +0900

    Title: 우리는 하나, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250626/우리는 하나/we-are-the-one.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static int n;
+    private static int n;
+    private static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokens = new StringTokenizer(reader.readLine());
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int u = sc.nextInt();
+        int d = sc.nextInt();
+        int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        
+    }
+}
