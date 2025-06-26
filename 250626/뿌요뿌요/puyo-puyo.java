371a7f5ef6700443efe163e17844fc3e5aa83562
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-06-26 12:05:53 +0900

    Title: 뿌요뿌요, Time: 100ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250626/뿌요뿌요/puyo-puyo.java
+++ 250626/뿌요뿌요/puyo-puyo.java
@@ -2,9 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    private static final int MAX = 100;
+    
     private static int n;
     private static int[][] grid;
+    private static int[][] visited = new int[MAX][MAX];
     private static int count;
+    private static int bombed;
+    private static int max;
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -18,9 +23,57 @@ public class Main {
                 grid[row][column] = Integer.parseInt(tokens.nextToken());
             }
         }
-            
-        find();
+
+        for (int row = 0; row < n; row++) {
+            for (int column = 0; column < n; column++) {
+                if (visited[row][column] == 0) {
+                    visited[row][column] = 1;
+                    count = 1;
+                    find(row, column);
+                    if (4 <= count) {
+                        bombed++;
+                    }
+                }
+            }
+        }
+        
+        System.out.println(bombed + " " + max);
     }
 
-    private static void 
+    private static void find(int x, int y) {
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{1, 0, -1, 0};
+
+        for (int i = 0; i < 4; i++) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+
+            if (isMovable(x, y, newX, newY)) {
+                visited[newX][newY] = 1;
+                count++;
+                max = Math.max(count, max);
+                find(newX, newY);
+            }
+        }
+    }
+
+    private static boolean isMovable(int x, int y, int newX, int newY) {
+        if (!isInRange(newX, newY)) {
+            return false;
+        }
+
+        if (visited[newX][newY] == 1) {
+            return false;
+        }
+
+        if (grid[x][y] != grid[newX][newY]) {
+            return false;
+        }
+    
+        return true;
+    }
+
+    private static boolean isInRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
 }
