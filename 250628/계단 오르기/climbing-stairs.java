8580af7a4d397c6a29f5e6834d8a4da64785bb78
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-06-28 10:05:51 +0900

    Title: 계단 오르기, Time: 92ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250628/계단 오르기/climbing-stairs.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final int MAX = 1_000;
+
+    private static int n;
+    private static int[] dp = new int[MAX + 1];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokens = new StringTokenizer(reader.readLine());
+        n = Integer.parseInt(tokens.nextToken());
+
+        /* bottom-up tabulation */
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        find();
+
+        System.out.println(dp[n]);
+    }
+
+    private static void find() {
+        for (int i = 4; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+    }
+}
