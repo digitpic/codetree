91151ce49bb753b07f1893c4fa7698c7546adb67
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-06-28 09:45:55 +0900

    Title: 피보나치 수, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

--- 250628/피보나치 수/fibonacci-number.java
+++ 250628/피보나치 수/fibonacci-number.java
@@ -1,6 +1,40 @@
 import java.util.*;
 import java.io.*;
 
+/* top-down memoization */
+public class Main {
+    private static final int MAX = 45;
+    private static final int UNUSED = -1;
+
+    private static int n;
+    private static int[] memo = new int[MAX + 1];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokens = new StringTokenizer(reader.readLine());
+        n = Integer.parseInt(tokens.nextToken());
+
+        for (int i = 0; i <= MAX; i++) {
+            memo[i] = UNUSED;
+        }
+
+        System.out.println(find(n));
+    }
+
+    private static int find(int number) {
+        if (memo[number] != UNUSED) {
+            return memo[number];
+        }
+        
+        if (number == 1 || number == 2) {
+            return memo[number] = 1;
+        }
+
+        return memo[number] = find(number - 1) + find(number - 2);
+    }
+}
+
+/* bottom-up tabulation
 public class Main {
     private static final int MAX = 45;
 
@@ -25,3 +59,4 @@ public class Main {
         }
     }
 }
+*/
