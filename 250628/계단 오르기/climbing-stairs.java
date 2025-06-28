ba2d391b529818825e33809a9bc1a9e0c64d961d
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-06-28 10:19:27 +0900

    Title: 계단 오르기, Time: 114ms, Memory: 9MB, Status: Passed - Codetree

--- 250628/계단 오르기/climbing-stairs.java
+++ 250628/계단 오르기/climbing-stairs.java
@@ -1,6 +1,43 @@
 import java.util.*;
 import java.io.*;
 
+/* top-down memoization */
+public class Main {
+    private static final int MAX = 1_000;
+
+    private static int n;
+    private static int[] memo = new int[MAX + 1];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokens = new StringTokenizer(reader.readLine());
+        n = Integer.parseInt(tokens.nextToken());
+
+        for (int i = 0; i <= MAX; i++) {
+            memo[i] = -1;
+        }
+
+        System.out.println(find(n));
+    }
+
+    private static int find(int number) {
+        if (memo[number] != -1) {
+            return memo[number];
+        }
+
+        if (number == 0 || number == 1) {
+            return memo[number] = 0;
+        }
+
+        if (number == 2 || number == 3) {
+            return memo[number] = 1;
+        }
+
+        return memo[number] = (find(number - 2) + find(number - 3)) % 10_007;
+    }
+}
+
+/* bottom-up tabulation
 public class Main {
     private static final int MAX = 1_000;
 
@@ -12,7 +49,6 @@ public class Main {
         StringTokenizer tokens = new StringTokenizer(reader.readLine());
         n = Integer.parseInt(tokens.nextToken());
 
-        /* bottom-up tabulation */
         dp[0] = 0;
         dp[1] = 0;
         dp[2] = 1;
@@ -29,3 +65,4 @@ public class Main {
         }
     }
 }
+*/
