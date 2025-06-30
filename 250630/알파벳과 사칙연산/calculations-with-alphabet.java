62d93cf69c6ee3fd64a49d28dda9383220532943
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-06-30 20:38:45 +0900

    Title: 알파벳과 사칙연산, Time: 137ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250630/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class Main {
+
+    private static int n;
+    private static char[] operand;
+    private static char[] operator;
+    private static List<Integer> selected = new ArrayList<>();
+    private static int max;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String expression = sc.next();
+        
+        n = expression.length();
+        operand = new char[n / 2 + 1];
+        operator = new char[n / 2];
+
+        int operandIndex = 0;
+        int operatorIndex = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                operand[operandIndex++] = expression.charAt(i);
+            }
+
+            if (i % 2 == 1) {
+                operator[operatorIndex++] = expression.charAt(i);
+            }            
+        }
+
+        find(0);
+
+        System.out.println(max);
+    }
+
+    private static void find(int count) {
+        if (count == n / 2 + 1) {
+            max = Math.max(max, calculate());
+            return;
+        }
+        
+        for (int i = 1; i <= 4; i++) {
+            selected.add(i);
+            find(count + 1);
+            selected.remove(selected.size() - 1);
+        }
+    }
+
+    private static int calculate() {
+        int result = selected.get(0);
+
+        for (int i = 1; i < selected.size(); i++) {
+            if (operator[i - 1] == '+') {
+                result += selected.get(i);
+            } else if (operator[i - 1] == '-') {
+                result -= selected.get(i) ;
+            } else if (operator[i - 1] == '*') {
+                result *= selected.get(i);
+            }
+        }
+
+        return result;
+    }
+}
