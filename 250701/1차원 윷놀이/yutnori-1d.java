bc22747dcd669efb660d9d4eaab2b7d3e3ac7b87
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-07-01 13:21:35 +0900

    Title: 1차원 윷놀이, Time: 88ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250701/1차원 윷놀이/yutnori-1d.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final int MAX_N = 12;
+    private static final int MAX_K = 4;
+
+    private static int n;
+    private static int m;
+    private static int k;
+    private static int max = Integer.MIN_VALUE;
+    private static int[] moves = new int[MAX_N];
+    private static int[] status = new int[MAX_K];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokens = new StringTokenizer(reader.readLine());
+
+        n = Integer.parseInt(tokens.nextToken());
+        m = Integer.parseInt(tokens.nextToken());
+        k = Integer.parseInt(tokens.nextToken());
+
+        tokens = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < n; i++) {
+            int move = Integer.parseInt(tokens.nextToken());
+            moves[i] = move;
+        }
+
+        for (int i = 0; i < k; i++) {
+            status[i]++;
+        }
+
+        find(0);
+        
+        System.out.println(max);
+    }
+
+    private static void find(int count) {
+        if (count == n) {
+            max = Math.max(max, calculate());
+            return;
+        }
+
+        for (int i = 0; i < k; i++) {
+            if (status[i] >= m) {
+                continue;
+            }
+
+            status[i] += moves[count];
+            find(count + 1);
+            status[i] -= moves[count];
+        }
+    }
+
+    private static int calculate() {
+        int point = 0;
+
+        for (int i = 0; i < k; i++) {
+            if (status[i] >= m) {
+                point++;
+            }
+        }
+
+        return point;
+    }
+}
