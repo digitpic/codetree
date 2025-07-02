8968608067eb444f5f16503335b3e1cba42d87e1
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-07-02 09:56:15 +0900

    Title: 최소 점프 횟수, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

--- 250702/최소 점프 횟수/min-num-of-jumps.java
+++ 250702/최소 점프 횟수/min-num-of-jumps.java
@@ -19,30 +19,27 @@ public class Main {
             jumps[i] = Integer.parseInt(tokens.nextToken());
         }
 
-        find(0);
+        find(0, 0);
+
+        if (min == Integer.MAX_VALUE) {
+            min = -1;
+        }
 
         System.out.println(min);
     }
 
-    private static void find(int count) {
-        if (count == n || step >= n) {            
-            min = Math.min(min, step);
+    private static void find(int position, int count) {
+        if (position >= n - 1) {
+            min = Math.min(min, count);
             return;
         }
         
-        for (int i = 0; i < n; i++) {
-            if (jumps[i] == 0) {
-                min = -1;
-                continue;
-            }
-            
-            step = 1;
-            
-            for (int j = 1; j <= jumps[i]; j++) {
-                step += j;
-                find(count + 1);
-                step -= j;
-            }
+        if (jumps[position] == 0) {
+                return;
+        }
+
+        for (int j = 1; j <= jumps[position]; j++) {
+            find(position + j, count + 1);
         }
     }
-}
+}   
