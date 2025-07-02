df9763150ba472db21323d98b9579da491b86a83
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-07-02 14:39:57 +0900

    Title: N개 중에 M개 뽑기, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250702/N개 중에 M개 뽑기/n-choose-m.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final int MAX = 10;
+
+    private static int n;
+    private static int m;
+    
+    private static List<Integer> numbers = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokens = new StringTokenizer(reader.readLine());
+        n = Integer.parseInt(tokens.nextToken());
+        m = Integer.parseInt(tokens.nextToken());
+
+        find(0, 0);    
+    }
+
+    private static void find(int count, int previous) {
+        if (numbers.size() == m) {
+            for (int i = 0; i < numbers.size(); i++) {
+                System.out.print(numbers.get(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (previous < i) {
+                numbers.add(i);
+                find(count + 1, i);
+                numbers.remove(numbers.size() - 1);
+            }
+        }
+    }
+}
