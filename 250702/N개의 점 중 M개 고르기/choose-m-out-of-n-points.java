91b29f53315e3622dcf53ac89cfc434a82aeb398
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-07-02 16:56:28 +0900

    Title: N개의 점 중 M개 고르기, Time: 200ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250702/N개의 점 중 M개 고르기/choose-m-out-of-n-points.java
@@ -0,0 +1,82 @@
+import java.util.*;
+import java.io.*;
+
+class Pair {
+    public int x;
+    public int y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    private static int n;
+    private static int m;
+    private static List<Pair> points = new ArrayList<>();
+    private static List<Pair> selected = new ArrayList<>();
+    private static List<Pair> pair = new ArrayList<>();
+
+    private static int min = Integer.MAX_VALUE;
+    private static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokens = new StringTokenizer(reader.readLine());
+
+        n = Integer.parseInt(tokens.nextToken());
+        m = Integer.parseInt(tokens.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            tokens = new StringTokenizer(reader.readLine());
+            int x = Integer.parseInt(tokens.nextToken());
+            int y = Integer.parseInt(tokens.nextToken());
+            points.add(new Pair(x, y));
+        }
+        
+        find(0, 0);
+
+        System.out.println(min);
+    }
+
+    private static void find(int index, int count) {
+        if (index == n) {
+            if (count == m) {
+                max = Integer.MIN_VALUE;
+                findPair(0, 0);
+                min = Math.min(min, max);
+            }
+
+            return;
+        }
+
+        selected.add(points.get(index));
+        find(index + 1, count + 1);
+        selected.remove(selected.size() - 1);
+
+        find(index + 1, count);
+    }
+
+    private static void findPair(int index, int count) {
+        if (index == m) {
+            if (count == 2) {
+                Pair first = pair.get(0);
+                Pair second = pair.get(1);
+                max = Math.max(max, calculate(first, second));
+            }
+            return;
+        }
+        
+        pair.add(selected.get(index));
+        findPair(index + 1, count + 1);
+        pair.remove(pair.size() - 1);
+
+        findPair(index + 1, count);
+    }
+
+    private static int calculate(Pair first, Pair second) {
+        return (int) Math.pow(first.x - second.x, 2)
+                + (int) Math.pow(first.y - second.y, 2);
+    }
+}
