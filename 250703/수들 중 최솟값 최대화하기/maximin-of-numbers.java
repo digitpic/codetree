9054bba105b5924d079dccd4a5710ad678f194b8
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-07-03 13:50:19 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 225ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250703/수들 중 최솟값 최대화하기/maximin-of-numbers.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final int MAX_N = 10;
+
+    private static int n;
+    private static int[][] grid = new int[MAX_N][MAX_N];
+    private static int[] visited = new int[MAX_N];
+    private static List<Integer> selected = new ArrayList<>();
+    private static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokens = new StringTokenizer(reader.readLine());
+
+        n = Integer.parseInt(tokens.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            tokens = new StringTokenizer(reader.readLine());
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = Integer.parseInt(tokens.nextToken());
+            }
+        }
+            
+        find(0);
+
+        System.out.println(max);
+    }
+
+    private static void find(int row) {
+        if (row == n) {
+            max = Math.max(max, calculate());
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 1) {
+                continue;
+            }
+
+            visited[i] = 1;
+            selected.add(i);
+
+            find(row + 1);
+
+            visited[i] = 0;
+            selected.remove(selected.size() - 1);
+        }
+    }
+
+    private static int calculate() {
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) { 
+            min = Math.min(min, grid[i][selected.get(i)]);
+        }
+
+        return min;
+    }
+}   
