bb441126144f1555d6492ebf07a17a1ddf712e7d
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-07-05 18:59:13 +0900

    Title: 정수 사각형 최대 합, Time: 102ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250705/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final int MAX_N = 100;
+
+    private static int n;
+    private static int[][] grid = new int[MAX_N][MAX_N];
+    private static int[][] dp = new int[MAX_N][MAX_N];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokens = new StringTokenizer(reader.readLine());
+
+        n = Integer.parseInt(tokens.nextToken());
+        for (int i = 0; i < n; i++) {
+            tokens = new StringTokenizer(reader.readLine());
+            for (int j = 0; j < n; j++) {
+                int value = Integer.parseInt(tokens.nextToken());
+                grid[i][j] = value;
+            }
+        }
+
+        dp[0][0] = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                if (dp[i][j] == 0) {
+                    int left = dp[i][j - 1] + grid[i][j];
+                    int top = dp[i - 1][j] + grid[i][j];
+                    dp[i][j] = Math.max(left, top);
+                }
+            }
+        }
+        
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, dp[n - 1][i]);
+        }
+
+        System.out.println(max);
+    }
+}
