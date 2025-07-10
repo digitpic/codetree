69eef6cc84846469277b8dee30d819148a60f12f
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-07-10 16:18:43 +0900

    Title: 부분 수열의 합이 M, Time: 149ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250710/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final int INT_MAX = Integer.MAX_VALUE;
+    private static final int MAX = 10_000;
+
+    private static int n;
+    private static int m;
+
+    private static int[] coin = new int[MAX + 1];
+    private static int[] dp = new int[MAX + 1];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokens = new StringTokenizer(reader.readLine());
+        n = Integer.parseInt(tokens.nextToken());
+        m = Integer.parseInt(tokens.nextToken());
+
+        tokens = new StringTokenizer(reader.readLine());
+        for (int i = 1; i <= n; i++) {
+            coin[i] = Integer.parseInt(tokens.nextToken());
+        }
+        
+        for (int i = 1; i <= MAX; i++) {
+            dp[i] = INT_MAX;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = m; j >= 0; j--) {
+                if (j >= coin[i]) {
+                    if (dp[j - coin[i]] == INT_MAX) {
+                        continue;
+                    }
+                    
+                    dp[j] = Math.min(dp[j], dp[j - coin[i]] + 1);
+                }
+            }
+        }        
+
+        if (dp[m] == INT_MAX) {
+            dp[m] = -1;
+        }
+        
+        System.out.println(dp[m]);
+    }
+}
