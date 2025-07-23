1dcf1d5559e6798a7e4312615ffd8f77376b2b9c
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-07-23 14:37:04 +0900

    Title: 가장 짧은 부분합, Time: 3024ms, Memory: 17MB, Status: Time Limit Exceed - Codetree

---
+++ 250723/가장 짧은 부분합/shortest-subtotal.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokens = new StringTokenizer(reader.readLine());
+
+        int n = Integer.parseInt(tokens.nextToken());
+        int s = Integer.parseInt(tokens.nextToken());
+        int[] numbers = new int[n];
+
+        tokens = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(tokens.nextToken());
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += numbers[j]; 
+                if (sum >= s) {
+                    min = Math.min(min, j - i + 1);
+                }
+            }
+        }
+
+        if (min == Integer.MAX_VALUE) {
+            min = -1;
+        }
+
+        System.out.println(min);
+    }
+}
